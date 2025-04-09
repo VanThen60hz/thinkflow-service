2f55a686eb867cc54adfd301815df7c83c53cb86
VanThen <nguyenvthang2409@gmail.com>
2025-04-09 18:05:07 +0700

    add cors

--- note-service/middleware/cors.go
+++ note-service/middleware/cors.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001, http://localhost:3002")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, X-Requested-With, Authorization")
